3e0abb86eb5ab844bacb1b48d3f0536ed850a7a3
dedechandran <dedechandra852.dc@gmail.com>
2019-10-28 09:45:15 +0700

    change movie and tvshow resources access

--- app/src/main/java/com/example/androidjetpacksubmission/utils/DummyData.kt
+++ app/src/main/java/com/example/androidjetpacksubmission/utils/DummyData.kt
@@ -4,7 +4,6 @@ import android.content.res.Resources
 import com.example.androidjetpacksubmission.R
 import com.example.androidjetpacksubmission.domain.Movie
 import com.example.androidjetpacksubmission.domain.TvShow
-import com.example.androidjetpacksubmission.fixtures.*
 import java.text.NumberFormat
 import javax.inject.Inject
 
@@ -14,34 +13,279 @@ class DummyData @Inject constructor(private val resources: Resources) {
         currency.minimumFractionDigits = 0
 
         val movies = mutableListOf<Movie>()
-        val moviePosters = resources.obtainTypedArray(R.array.movie_posters)
-        val movieTitles = resources.getStringArray(R.array.movie_titles)
-        val movieOverviews = resources.getStringArray(R.array.movie_overviews)
-        val movieReleaseDates = resources.getStringArray(R.array.movie_release_dates)
-        val movieLanguages = resources.getStringArray(R.array.movie_languages)
-        val movieDurations = resources.getStringArray(R.array.movie_durations)
-        val movieGenres = resources.getStringArray(R.array.movie_genres)
-        val movieBudgets = resources.getStringArray(R.array.movie_budgets)
-        val movieRevenues = resources.getStringArray(R.array.movie_revenues)
-
-        for((index,movie) in movieTitles.withIndex()){
-            movies.add(
-                Movie(
-                    movieId = index,
-                    movieTitle = movie,
-                    moviePoster = moviePosters.getResourceId(index,0),
-                    movieOverview = movieOverviews[index],
-                    movieReleaseDate = movieReleaseDates[index],
-                    movieLanguage = movieLanguages[index],
-                    movieDuration = movieDurations[index],
-                    movieGenres = movieGenres[index].split(';'),
-                    movieBudget = currency.format(movieBudgets[index].toLong()),
-                    movieRevenue = currency.format(movieRevenues[index].toLong())
-                )
+        movies.add(
+            Movie(
+                movieId = 0,
+                movieTitle = "A Star Is Born",
+                moviePoster = R.drawable.mv_poster_a_star_is_born,
+                movieOverview = "Seasoned musician Jackson Maine discovers and falls in love with struggling artist Ally. She has just about given up on her dream to make it big as a singer until Jack coaxes her into the spotlight. But even as Allys career takes off, the personal side of their relationship is breaking down, as Jack fights an ongoing battle with his own internal demons.",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
             )
-        }
+        )
 
-        moviePosters.recycle()
+        movies.add(
+            Movie(
+                movieId = 1,
+                movieTitle = "Alita",
+                moviePoster = R.drawable.mv_poster_alita,
+                movieOverview = "When Alita awakens with no memory of who she is in a future world she does not recognize, she is taken in by Ido, a compassionate doctor who realizes that somewhere in this abandoned cyborg shell is the heart and soul of a young woman with an extraordinary past.",
+                movieReleaseDate = "January 31, 2019",
+                movieLanguage = "English",
+                movieDuration = "2h 2m",
+                movieGenres = "action;science fiction;thriller;adventure",
+                movieBudget = "170000000",
+                movieRevenue = "404852543"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 2,
+                movieTitle = "Aquaman",
+                moviePoster = R.drawable.mv_poster_aquaman,
+                movieOverview = "Once home to the most advanced civilization on Earth, Atlantis is now an underwater kingdom ruled by the power-hungry King Orm. With a vast army at his disposal, Orm plans to conquer the remaining oceanic people and then the surface world. Standing in his way is Arthur Curry, Orms half-human, half-Atlantean brother and true heir to the throne.",
+                movieReleaseDate = "December 7, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 24m",
+                movieGenres = "action;adventure;fantasy",
+                movieBudget = "160000000",
+                movieRevenue = "1143689193"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 3,
+                movieTitle = "Bohemian Rhapsody",
+                moviePoster = R.drawable.mv_poster_bohemian,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 4,
+                movieTitle = "Cold Pursuit",
+                moviePoster = R.drawable.mv_poster_cold_pursuit,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 5,
+                movieTitle = "Creed II",
+                moviePoster = R.drawable.mv_poster_creed,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 6,
+                movieTitle = "Fantastic Beasts: The Crimes of Grindelwald",
+                moviePoster = R.drawable.mv_poster_crimes,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 7,
+                movieTitle = "Glass",
+                moviePoster = R.drawable.mv_poster_glass,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 8,
+                movieTitle = "How to Train Your Dragon",
+                moviePoster = R.drawable.mv_poster_how_to_train,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 9,
+                movieTitle = "Avengers: Infinoty War",
+                moviePoster = R.drawable.mv_poster_infinity_war,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 10,
+                movieTitle = "Mary Queen of Scots",
+                moviePoster = R.drawable.mv_poster_marry_queen,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 11,
+                movieTitle = "Mortal Engines",
+                moviePoster = R.drawable.mv_poster_mortal_engines,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 12,
+                movieTitle = "Overlord",
+                moviePoster = R.drawable.mv_poster_a_star_is_born,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 0,
+                movieTitle = "A Star Is Born",
+                moviePoster = R.drawable.mv_poster_a_star_is_born,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 0,
+                movieTitle = "A Star Is Born",
+                moviePoster = R.drawable.mv_poster_a_star_is_born,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 0,
+                movieTitle = "A Star Is Born",
+                moviePoster = R.drawable.mv_poster_a_star_is_born,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 0,
+                movieTitle = "A Star Is Born",
+                moviePoster = R.drawable.mv_poster_a_star_is_born,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+        movies.add(
+            Movie(
+                movieId = 0,
+                movieTitle = "A Star Is Born",
+                moviePoster = R.drawable.mv_poster_a_star_is_born,
+                movieOverview = "",
+                movieReleaseDate = "October 3, 2018",
+                movieLanguage = "English",
+                movieDuration = "2h 15m",
+                movieGenres = "drama;romance;music",
+                movieBudget = "36000000",
+                movieRevenue = "170000000"
+            )
+        )
+
+
+
+
         return movies
     }
 
@@ -56,12 +300,12 @@ class DummyData @Inject constructor(private val resources: Resources) {
         val tvShowGenres = resources.getStringArray(R.array.tv_show_genres)
         val tvShowTypes = resources.getStringArray(R.array.tv_show_types)
 
-        for((index,tvShow) in tvShowTitles.withIndex()){
+        for ((index, tvShow) in tvShowTitles.withIndex()) {
             tvShows.add(
                 TvShow(
                     tvShowId = index,
                     tvShowTitle = tvShow,
-                    tvShowPoster = tvShowPosters.getResourceId(index,0),
+                    tvShowPoster = tvShowPosters.getResourceId(index, 0),
                     tvShowOverview = tvShowOverviews[index],
                     tvShowReleaseDate = tvShowReleaseDates[index],
                     tvShowLanguage = tvShowLanguages[index],
