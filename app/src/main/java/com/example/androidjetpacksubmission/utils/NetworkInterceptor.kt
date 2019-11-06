c46f484a0f08e89b5ab8822bb0f88e761f822bac
dedechandran <dedechandra852.dc@gmail.com>
2019-11-06 12:48:38 +0700

    move apikey

--- app/src/main/java/com/example/androidjetpacksubmission/utils/NetworkInterceptor.kt
+++ app/src/main/java/com/example/androidjetpacksubmission/utils/NetworkInterceptor.kt
@@ -1,14 +1,16 @@
 package com.example.androidjetpacksubmission.utils
 
+import com.example.androidjetpacksubmission.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 
 class NetworkInterceptor @Inject constructor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        val apiKey = BuildConfig.API_KEY
         val url = chain.request().url
             .newBuilder()
-            .addQueryParameter("api_key","a4efaa7ae55e845278da0fd4549e3246")
+            .addQueryParameter("api_key",apiKey)
             .build()
         val request = chain.request().newBuilder().url(url).build()
         return chain.proceed(request)
